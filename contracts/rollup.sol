c3a36cc81a156f5c6afa9f8ad634249e34e94af6
vaibhavchellani <vaibhavchellani223@gmail.com>
2019-11-19 03:40:07 +0530

    attached merkel lib to root contract

--- contracts/rollup.sol
+++ contracts/rollup.sol
@@ -1,8 +1,13 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
-contract Rollup {
-    // batch 
+import {MerkleTree as MerkleTreeUtil} from "./merkelTreeLib.sol";
+
+
+contract Rollup is MerkleTreeUtil{
+    constructor() MerkleTreeUtil(2,4) public{
+    }
+    // Batch
     struct Batch{
         bytes32 stateRoot;
         bytes32 withdraw_root;
@@ -10,17 +15,15 @@ contract Rollup {
         bytes32 account_tree_state;
         uint timestamp;
     }
-    
     Batch[] public batches;
     bytes32 public ACCOUNT_TREE_STATE = 0x0000000000000000000000000000000000000000000000000000000000000000;
-
     bytes32 public ZERO_BYTES32 = 0x0000000000000000000000000000000000000000000000000000000000000000;
 
-    
+    function updateTx(bytes memory tx_sig,address tx_from,address tx_to,uint256 tx_amount,bytes memory proof_from,bytes memory proof_to) public {
+        
+    }
     function submitBatch(bytes[] calldata _txs,bytes32 updatedRoot) external returns(bytes32) {
      // make merkel root of all txs
-     
-    
      Batch memory newBatch = Batch({
         stateRoot: updatedRoot,
         committer: msg.sender,
@@ -28,7 +31,10 @@ contract Rollup {
         withdraw_root: ACCOUNT_TREE_STATE,
         timestamp: now
      });
+     MerkleTreeUtil.insert(newBatch);
      
      batches.push(newBatch);
     }
+    
+    
 }
