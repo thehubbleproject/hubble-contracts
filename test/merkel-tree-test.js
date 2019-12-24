d426714b5f777ef7b5e7d0a1e2b25708dca2ebdb
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2019-12-24 22:12:47 +0530

    added odd number of leaves test case

--- test/merkel-tree-test.js
+++ test/merkel-tree-test.js
@@ -4,11 +4,21 @@ var BigNumber = require("big-number");
 const MerkleTree = artifacts.require("MerkleTree");
 
 contract("MerkleTree", async accounts => {
-  it("we should be able to obtain a merkle root", async () => {
+  it("create a merkle root with even number of elements", async () => {
     let mt = await MerkleTree.deployed();
     var variable1 = ethUtils.bufferToHex("0x12")
     var variable2 = ethUtils.bufferToHex("0x12")
     var result = await mt.getMerkleRoot([variable1,variable2])
+    console.log("merkle tree root obtained",result)
+  });
+
+  it("create a merkle root with odd number of elements", async () => {
+    let mt = await MerkleTree.deployed();
+    var variable1 = ethUtils.bufferToHex("0x12")
+    var variable2 = ethUtils.bufferToHex("0x12")
+    var variable3 = ethUtils.bufferToHex("0x12")
+
+    var result = await mt.getMerkleRoot([variable1,variable2,variable3])
     console.log("merkle tree root obtained",result)
   });
 });
