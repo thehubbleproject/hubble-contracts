f22c4134929b370a0279b8e97e3bc5db86f7d657
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2020-03-23 01:55:55 +0530

    added contract addresses file creation

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,13 +1,19 @@
 const MTLib = artifacts.require("MerkleTreeLib");
+
 const MT = artifacts.require("MerkleTree");
 const RollUp = artifacts.require("Rollup");
 const ECVerify = artifacts.require("ECVerify");
 const TokenRegistry = artifacts.require("TokenRegistry");
 const TestToken = artifacts.require("TestToken");
-// var Web3 = require('web3');
-// var web3 = new Web3();
 
+const fs = require('fs')
 
+function writeContractAddresses(contractAddresses) {
+  fs.writeFileSync(
+    `${process.cwd()}/contractAddresses.json`,
+    JSON.stringify(contractAddresses, null, 2) // Indent 2 spaces
+  )
+}
 module.exports = async function(deployer) {
   // picked address from mnemoic
   var coordinator = "0x9fB29AAc15b9A4B7F17c3385939b007540f4d791"
@@ -24,7 +30,21 @@ module.exports = async function(deployer) {
   
   tokenRegistry = await deployer.deploy(TokenRegistry,coordinator);
   
-  await deployer.deploy(RollUp,balanceTree.address,accountTree.address,mtLibAddr,tokenRegistry.address,coordinator);
+rollupContract =   await deployer.deploy(RollUp,balanceTree.address,accountTree.address,mtLibAddr,tokenRegistry.address,coordinator);
   
   await deployer.deploy(TestToken,coordinator);
+            console.log('writing contract addresses to file...')
+
+    const contractAddresses = {
+            Coordinator: coordinator,
+        MerkleTreeLib: MTLib.address,
+        BalanceTree: balanceTree.address,
+        AccountTree: accountTree.address,
+        ECVeridy: ECVerify.address,
+        TokenRegistry: TokenRegistry.address,
+            TestTokenContract: TestToken.address,
+        RollupContract: rollupContract.address,
+    }
+    writeContractAddresses(contractAddresses)
+        
 };
