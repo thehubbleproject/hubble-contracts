4700c60e4387e934d860caf2a5e1c4a8f242e757
0xAshish <a4ashishrp@gmail.com>
2020-06-18 17:32:01 +0530

    add coverage for tests

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  providerOptions: {
+    port: 8555,
+    gasPrice: 0,
+    blockTime: 1,
+    mnemonic: 'clock radar mass judge dismiss just intact mind resemble fringe diary casino',
+    total_accounts: 10
+  }
+}
