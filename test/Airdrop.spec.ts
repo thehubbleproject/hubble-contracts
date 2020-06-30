3d4f95a3718aefdc91391e8ebba7921bfa3649bb
Chih Cheng Liang <chihchengliang@gmail.com>
2020-06-30 19:10:45 +0800

    remove unused

--- test/Airdrop.spec.ts
+++ test/Airdrop.spec.ts
@@ -1,4 +1,3 @@
-import { createAirdropBatch } from '../offchain/airdrop'
 import { ethers } from "ethers";
 const RollupCore = artifacts.require("Rollup");
 
@@ -6,17 +5,12 @@ describe("Rollup Airdrop", async function () {
 
     it("lets coordinator submit a batch", async function () {
         const rollupCoreInstance = await RollupCore.deployed()
-
-        const root = await rollupCoreInstance.getLatestBalanceTreeRoot()
-        expect(root.startsWith('0x')).is.true
-
         await rollupCoreInstance.submitBatch(
             ["0xabc", "0xabc"],
             "0xb6b4b5c6cb43071b3913b1d500b33c52392f7aa85f8a451448e20c3967f2b21a",
             0,
             { value: ethers.utils.parseEther("32").toString() },
         )
-
 
         // create drops 
 
