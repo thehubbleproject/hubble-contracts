62596cb753238bc61a4cc4e7764d0b3bf750f00c
Chih Cheng Liang <chihchengliang@gmail.com>
2020-07-17 21:15:37 +0800

    move submitbatch to rollup.sol

--- contracts/rollupReddit.sol
+++ contracts/rollupReddit.sol
@@ -5,10 +5,9 @@ import {IReddit} from "./interfaces/IReddit.sol";
 import {ParamManager} from "./libs/ParamManager.sol";
 import {Types} from "./libs/Types.sol";
 import {NameRegistry as Registry} from "./NameRegistry.sol";
-import {RollupHelpers} from "./rollup.sol";
 import {RollupUtils} from "./libs/RollupUtils.sol";
 
-contract RollupReddit is RollupHelpers {
+contract RollupReddit {
     Registry public nameRegistry;
     IReddit public createAccount;
     IReddit public airdrop;
@@ -53,7 +52,7 @@ contract RollupReddit is RollupHelpers {
         )
     {
         Types.DropTx memory _tx = RollupUtils.AirdropTxFromBytes(txBytes);
-               _tx.signature = sig;
+        _tx.signature = sig;
         return
             airdrop.processAirdropTx(
                 _balanceRoot,
@@ -68,7 +67,9 @@ contract RollupReddit is RollupHelpers {
         Types.AccountMerkleProof memory _merkle_proof,
         bytes memory txBytes
     ) public view returns (bytes memory, bytes32 newRoot) {
-        Types.DropTx memory transaction = RollupUtils.AirdropTxFromBytes(txBytes);
+        Types.DropTx memory transaction = RollupUtils.AirdropTxFromBytes(
+            txBytes
+        );
         return airdrop.ApplyAirdropTx(_merkle_proof, transaction);
     }
 
@@ -106,7 +107,7 @@ contract RollupReddit is RollupHelpers {
         )
     {
         Types.Transaction memory _tx = RollupUtils.TxFromBytes(txBytes);
-               _tx.signature = sig;
+        _tx.signature = sig;
         return
             transfer.processTx(
                 _balanceRoot,
@@ -169,59 +170,5 @@ contract RollupReddit is RollupHelpers {
                 _from_pda_proof,
                 accountProofs
             );
-    }
-
-    /**
-     * @notice Submits a new batch to batches
-     * @param _txs Compressed transactions .
-     * @param _updatedRoot New balance tree root after processing all the transactions
-     */
-    function submitBatch(
-        bytes[] calldata _txs,
-        bytes32 _updatedRoot,
-        Types.Usage batchType
-    ) external payable onlyCoordinator isNotRollingBack {
-        require(
-            msg.value >= governance.STAKE_AMOUNT(),
-            "Not enough stake committed"
-        );
-
-        require(
-            _txs.length <= governance.MAX_TXS_PER_BATCH(),
-            "Batch contains more transations than the limit"
-        );
-        bytes32 txRoot = merkleUtils.getMerkleRoot(_txs);
-        require(
-            txRoot != ZERO_BYTES32,
-            "Cannot submit a transaction with no transactions"
-        );
-        addNewBatch(txRoot, _updatedRoot, batchType);
-    }
-
-    /**
-     * @notice finalise deposits and submit batch
-     */
-    function finaliseDepositsAndSubmitBatch(
-        uint256 _subTreeDepth,
-        Types.AccountMerkleProof calldata _zero_account_mp
-    ) external payable onlyCoordinator isNotRollingBack {
-        bytes32 depositSubTreeRoot = depositManager.finaliseDeposits(
-            _subTreeDepth,
-            _zero_account_mp,
-            getLatestBalanceTreeRoot()
-        );
-        // require(
-        //     msg.value >= governance.STAKE_AMOUNT(),
-        //     "Not enough stake committed"
-        // );
-
-        bytes32 updatedRoot = merkleUtils.updateLeafWithSiblings(
-            depositSubTreeRoot,
-            _zero_account_mp.accountIP.pathToAccount,
-            _zero_account_mp.siblings
-        );
-
-        // add new batch
-        addNewBatchWithDeposit(updatedRoot, depositSubTreeRoot);
     }
 }
