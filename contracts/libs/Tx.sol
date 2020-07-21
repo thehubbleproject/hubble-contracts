be700d59267bc8c27e35baa38c125184c5bf454d
kilic <kiliconu@itu.edu.tr>
2020-07-21 14:40:47 +0300

    add create tx serialization methods

--- contracts/libs/Tx.sol
+++ contracts/libs/Tx.sol
@@ -10,6 +10,9 @@ library Tx {
     uint256 public constant MASK_NONCE = 0xffffffff;
     uint256 public constant MASK_TOKEN_ID = 0xffff;
 
+    uint256 constant bound4Bytes = 0x10000000000000000;
+    uint256 constant bound2Bytes = 0x100000000;
+
     // transaction_type: transfer
     // [sender_state_id<4>|receiver_state_id<4>|amount<4>|nonce<4>]
     uint256 public constant TX_LEN_0 = 16;
@@ -20,11 +23,26 @@ library Tx {
     uint256 public constant POSITION_AMOUNT_0 = 12;
     uint256 public constant POSITION_NONCE_0 = 16;
 
+    // transaction_type: create
+    // [receiver_account_id<4>|receiver_state_id<4>|token<2>}
+    uint256 public constant TX_LEN_1 = 10;
+    uint256 public constant MASK_TX_1 = 0xffffffffffffffffffff;
+    // positions in bytes
+    uint256 public constant POSITION_ACCOUNT_1 = 4;
+    uint256 public constant POSITION_STATE_1 = 8;
+    uint256 public constant POSITION_TOKEN_1 = 10;
+
     struct TransferDecoded {
         uint256 senderID;
         uint256 receiverID;
         uint256 amount;
         uint256 nonce;
+    }
+
+    struct CreateDecoded {
+        uint256 accountID;
+        uint256 stateID;
+        uint256 token;
     }
 
     function serialize(TransferDecoded[] memory txs)
@@ -33,17 +51,16 @@ library Tx {
         returns (bytes memory)
     {
         uint256 batchSize = txs.length;
-        uint256 bound = 0x10000000000000000;
         bytes memory serialized = new bytes(TX_LEN_0 * batchSize);
         for (uint256 i = 0; i < batchSize; i++) {
             uint256 sender = txs[i].senderID;
             uint256 receiver = txs[i].receiverID;
             uint256 amount = txs[i].amount;
             uint256 nonce = txs[i].nonce;
-            require(sender < bound, "invalid sender index");
-            require(receiver < bound, "invalid receiver index");
-            require(amount < bound, "invalid amount");
-            require(nonce < bound, "invalid amount");
+            require(sender < bound4Bytes, "invalid senderID");
+            require(receiver < bound4Bytes, "invalid receiverID");
+            require(amount < bound4Bytes, "invalid amount");
+            require(nonce < bound4Bytes, "invalid nonce");
             bytes memory _tx = abi.encodePacked(
                 uint32(sender),
                 uint32(receiver),
@@ -63,13 +80,11 @@ library Tx {
         pure
         returns (bool)
     {
-        uint256 txSize = txs.length / TX_LEN_0;
-        return txSize * TX_LEN_0 != txs.length;
+        return txs.length % TX_LEN_0 != 0;
     }
 
     function transfer_size(bytes memory txs) internal pure returns (uint256) {
-        uint256 txSize = txs.length / TX_LEN_0;
-        return txSize;
+        return txs.length / TX_LEN_0;
     }
 
     function transfer_senderOf(bytes memory txs, uint256 index)
@@ -161,5 +176,107 @@ library Tx {
             r := keccak256(p_tx, TX_LEN_0)
         }
         return BLS.mapToPoint(r);
+    }
+
+    function serialize(CreateDecoded[] memory txs)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        uint256 batchSize = txs.length;
+
+        bytes memory serialized = new bytes(TX_LEN_1 * batchSize);
+        for (uint256 i = 0; i < batchSize; i++) {
+            uint256 accountID = txs[i].accountID;
+            uint256 stateID = txs[i].stateID;
+            uint256 token = txs[i].token;
+            require(accountID < bound4Bytes, "invalid accountID");
+            require(stateID < bound4Bytes, "invalid stateID");
+            require(token < bound2Bytes, "invalid amount");
+            bytes memory _tx = abi.encodePacked(
+                uint32(accountID),
+                uint32(stateID),
+                uint16(token)
+            );
+            uint256 off = i * TX_LEN_1;
+            for (uint256 j = 0; j < TX_LEN_1; j++) {
+                serialized[j + off] = _tx[j];
+            }
+        }
+        return serialized;
+    }
+
+    function create_hasExcessData(bytes memory txs)
+        internal
+        pure
+        returns (bool)
+    {
+        return txs.length % TX_LEN_1 != 0;
+    }
+
+    function create_size(bytes memory txs) internal pure returns (uint256) {
+        return txs.length / TX_LEN_1;
+    }
+
+    function create_accountIdOf(bytes memory txs, uint256 index)
+        internal
+        pure
+        returns (uint256 receiver)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let p_tx := add(txs, mul(index, TX_LEN_1))
+            receiver := and(mload(add(p_tx, POSITION_ACCOUNT_1)), MASK_STATE_ID)
+        }
+    }
+
+    function create_stateIdOf(bytes memory txs, uint256 index)
+        internal
+        pure
+        returns (uint256 sender)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let p_tx := add(txs, mul(index, TX_LEN_1))
+            sender := and(mload(add(p_tx, POSITION_STATE_1)), MASK_STATE_ID)
+        }
+    }
+
+    function create_tokenOf(bytes memory txs, uint256 index)
+        internal
+        pure
+        returns (uint256 amount)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let p_tx := add(txs, mul(index, TX_LEN_1))
+            amount := and(mload(add(p_tx, POSITION_TOKEN_1)), MASK_TOKEN_ID)
+        }
+        return amount;
+    }
+
+    function create_hashOf(bytes memory txs, uint256 index)
+        internal
+        pure
+        returns (bytes32 result)
+    {
+        // solium-disable-next-line security/no-inline-assembly
+        assembly {
+            let p_tx := add(txs, add(mul(index, TX_LEN_1), 32))
+            result := keccak256(p_tx, TX_LEN_1)
+        }
+    }
+
+    function create_toLeafs(bytes memory txs)
+        internal
+        pure
+        returns (bytes32[] memory)
+    {
+        uint256 batchSize = create_size(txs);
+        bytes32[] memory buf = new bytes32[](batchSize);
+        for (uint256 i = 0; i < batchSize; i++) {
+            buf[i] = create_hashOf(txs, i);
+        }
+        return buf;
     }
 }
