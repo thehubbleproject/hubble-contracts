0f0a0389595916675547033dc9eb10d969ec495f
Chih Cheng Liang <chihchengliang@gmail.com>
2020-07-21 23:52:13 +0800

    remove unwanted

--- migrations/3_rollup-utils.js
+++ migrations/3_rollup-utils.js
@@ -6,7 +6,5 @@ const rollupUtilsLib = artifacts.require("RollupUtils");
 module.exports = async function (deployer) {
   if (argv.dn && argv.dn == 3) {
     await deployer.deploy(rollupUtilsLib);
-    const instance = await rollupUtilsLib.deployed;
-    console.log("rollupUtilsLibAddr :", instance.address)
   }
 };
