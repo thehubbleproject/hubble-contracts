b99e39e7d13e5f38ae957a0bc5e766509b0472e0
Chih Cheng Liang <chihchengliang@gmail.com>
2020-08-04 16:35:34 +0800

    feed real data

--- scripts/submitBatch.js
+++ scripts/submitBatch.js
@@ -1,14 +1,17 @@
 const { ethers } = require("ethers");
 const contractAddresses = require("../contractAddresses.json");
+const path = require("path");
 
 const RollupContract = require("../build/contracts/Rollup.json");
+const GovernanceContract = require("../build/contracts/Governance.json");
 
 const argv = require("minimist")(process.argv.slice(2), {
     string: ["batches"]
 });
 
 /*
-    $ node ./scripts/submitBatch.js --batches batches.json
+    $ wget https://gist.githubusercontent.com/vaibhavchellani/3c1f19925cf2d3a26edf218ff7cff33d/raw/7f4234f7daa6e99043f095df477d93c76708c504/commitments.json
+    $ node ./scripts/submitBatch.js --batches commitments.json
 */
 
 async function main() {
@@ -21,23 +24,44 @@ async function main() {
         RollupContract.abi,
         signer
     );
+    const rawJSON = require(path.join(process.cwd(), batchesFile));
 
-    const batches = [];
+    const batches = rawJSON.users;
+    const governaceInstance = new ethers.Contract(
+        contractAddresses.Governance,
+        GovernanceContract.abi,
+        signer
+    );
+    const stakeAmount = await governaceInstance.STAKE_AMOUNT();
+    console.log(
+        "stake amount is",
+        ethers.utils.formatEther(stakeAmount),
+        "ether"
+    );
 
-    for (const [index, batch] of batches.entries()) {
-        txs = "";
-        _updatedRoot = "";
-        batchType = 0;
+    for (const batch of batches) {
+        console.log("Processing batch", batch.index);
+        if (batch.root === "") {
+            console.log("Batch", batch.index, "is empty, skip.");
+            console.log(batch);
+            continue;
+        }
 
-        const tx = await rollupInstance.submitBatch(
-            txs,
-            _updatedRoot,
-            batchType
-        );
-        // Do callback here to unblock next tx
-        tx.wait().then(x => {
-            console.log("submited batch", index, "tx:", x);
-        });
+        try {
+            const tx = await rollupInstance.submitBatch(
+                `0x${batch.txs}`,
+                batch.root,
+                batch.type,
+                { value: stakeAmount }
+            );
+            // Do callback here to unblock next tx
+            tx.wait().then(receipt => {
+                console.log("submited batch", batch.index);
+            });
+        } catch (err) {
+            console.error(err.message);
+            console.log(batch);
+        }
     }
 }
 
