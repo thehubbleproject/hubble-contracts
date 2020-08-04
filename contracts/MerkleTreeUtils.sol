1321a5f6c977e7c36e298247c9c995d86bc96245
Chih Cheng Liang <chihchengliang@gmail.com>
2020-08-04 21:36:52 +0800

    fix getMerkleRoot too

--- contracts/MerkleTreeUtils.sol
+++ contracts/MerkleTreeUtils.sol
@@ -80,38 +80,12 @@ contract MerkleTreeUtils {
         view
         returns (bytes32)
     {
-        uint256 nextLevelLength = _dataBlocks.length;
-        uint256 currentLevel = 0;
-        bytes32[] memory nodes = new bytes32[](nextLevelLength + 1); // Add one in case we have an odd number of leaves
+        bytes32[] memory nodes = new bytes32[](_dataBlocks.length); // Add one in case we have an odd number of leaves
         // Generate the leaves
         for (uint256 i = 0; i < _dataBlocks.length; i++) {
             nodes[i] = keccak256(_dataBlocks[i]);
         }
-        if (_dataBlocks.length == 1) {
-            return nodes[0];
-        }
-        // Add a defaultNode if we've got an odd number of leaves
-        if (nextLevelLength % 2 == 1) {
-            nodes[nextLevelLength] = defaultHashes[currentLevel];
-            nextLevelLength += 1;
-        }
-
-        // Now generate each level
-        while (nextLevelLength > 1) {
-            currentLevel += 1;
-            // Calculate the nodes for the currentLevel
-            for (uint256 i = 0; i < nextLevelLength / 2; i++) {
-                nodes[i] = getParent(nodes[i * 2], nodes[i * 2 + 1]);
-            }
-            nextLevelLength = nextLevelLength / 2;
-            // Check if we will need to add an extra node
-            if (nextLevelLength % 2 == 1 && nextLevelLength != 1) {
-                nodes[nextLevelLength] = defaultHashes[currentLevel];
-                nextLevelLength += 1;
-            }
-        }
-        // Alright! We should be left with a single node! Return it...
-        return nodes[0];
+        return getMerkleRootFromLeaves(nodes);
     }
 
     /**
