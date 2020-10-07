979c98ac9e8002c9f944eb44a8ba1e3f8103d507
Chih Cheng Liang <chihchengliang@gmail.com>
2020-10-07 16:54:56 +0800

    test is last

--- contracts/test/TestMerkleTree.sol
+++ contracts/test/TestMerkleTree.sol
@@ -3,7 +3,9 @@ pragma experimental ABIEncoderV2;
 
 import { MerkleTreeUtilsLib, MerkleTreeUtils } from "../MerkleTreeUtils.sol";
 
-contract TestMerkleTree {
+contract TestMerkleTree is MerkleTreeUtils {
+    constructor(uint256 maxDepth) public MerkleTreeUtils(maxDepth) {}
+
     function testVerify(
         bytes32 root,
         bytes32 leaf,
@@ -17,6 +19,17 @@ contract TestMerkleTree {
             path,
             witnesses
         );
+        return (result, gasCost - gasleft());
+    }
+
+    function testIsLast(
+        bytes32 root,
+        bytes32 leaf,
+        uint256 path,
+        bytes32[] memory witnesses
+    ) public returns (bool, uint256) {
+        uint256 gasCost = gasleft();
+        bool result = isLast(root, leaf, path, witnesses);
         return (result, gasCost - gasleft());
     }
 }
--- test/merkleTree.test.ts
+++ test/merkleTree.test.ts
@@ -1,9 +1,10 @@
 import { randomLeaves } from "../ts/utils";
 import { assert } from "chai";
 import { ethers } from "@nomiclabs/buidler";
-import { Tree } from "../ts/tree";
+import { Hasher, Tree } from "../ts/tree";
 import { TestMerkleTreeFactory } from "../types/ethers-contracts/TestMerkleTreeFactory";
 import { TestMerkleTree } from "../types/ethers-contracts/TestMerkleTree";
+import { ZERO_BYTES32 } from "../ts/constants";
 
 // Test all stateless operations
 describe("MerkleTreeUtils", async function() {
@@ -11,7 +12,7 @@ describe("MerkleTreeUtils", async function() {
     let contract: TestMerkleTree;
     before(async function() {
         const [signer] = await ethers.getSigners();
-        contract = await new TestMerkleTreeFactory(signer).deploy();
+        contract = await new TestMerkleTreeFactory(signer).deploy(MAX_DEPTH);
     });
     it("verify", async function() {
         const size = 50;
@@ -33,5 +34,38 @@ describe("MerkleTreeUtils", async function() {
             totalCost += gasCost.toNumber();
         }
         console.log("Average cost of verifying a leaf", totalCost / size);
+    });
+    it("verify the last leaf", async function() {
+        const round = 33;
+        let totalCost = 0;
+        let size = 1;
+
+        for (let i = 0; i < round; i++) {
+            size += 1;
+            const leaves = randomLeaves(size);
+            const tree = Tree.new(MAX_DEPTH, Hasher.new("bytes", ZERO_BYTES32));
+            tree.updateBatch(0, leaves);
+            for (const [path, leaf] of leaves.entries()) {
+                const {
+                    0: result,
+                    1: gasCost
+                } = await contract.callStatic.testIsLast(
+                    tree.root,
+                    leaf,
+                    path,
+                    tree.witness(path).nodes
+                );
+                if (path == size - 1) {
+                    assert.isTrue(result, "Should be the last element");
+                    totalCost += gasCost.toNumber();
+                } else {
+                    assert.isFalse(result, "Should not be the last element");
+                }
+            }
+        }
+        console.log(
+            "Average cost of verifying a last leaf (success case)",
+            Math.ceil(totalCost / round)
+        );
     });
 });
