4c4ce322fbdedac22c9ce4c4e7f1124e697c3535
Chih Cheng Liang <chihchengliang@gmail.com>
2020-10-12 18:18:56 +0800

    rename gov functions

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -12,30 +12,30 @@ contract Governance {
         govTimeToFinalise = timeToFinalise;
     }
 
-    function MAX_DEPOSIT_SUBTREE() public view returns (uint256) {
+    function maxDepositSubtree() public view returns (uint256) {
         return govMaxDepositSubtree;
     }
 
-    function TIME_TO_FINALISE() public view returns (uint256) {
+    function timeToFinalise() public view returns (uint256) {
         return govTimeToFinalise;
     }
 
     // min gas required before rollback pauses
     uint256 public govMinGasLeft = 100000;
 
-    function MIN_GAS_LIMIT_LEFT() public view returns (uint256) {
+    function minGasLeft() public view returns (uint256) {
         return govMinGasLeft;
     }
 
     uint256 public govMaxTxsPerBatch = 32;
 
-    function MAX_TXS_PER_BATCH() public view returns (uint256) {
+    function maxTxsPerBatch() public view returns (uint256) {
         return govMaxTxsPerBatch;
     }
 
     uint256 public govStakeAmount = 0.1 ether;
 
-    function STAKE_AMOUNT() public view returns (uint256) {
+    function stakeAmount() public view returns (uint256) {
         return govStakeAmount;
     }
 }
