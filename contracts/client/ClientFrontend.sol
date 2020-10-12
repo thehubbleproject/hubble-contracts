97411a84cfbf29c4c2ab7863fd4ccf262a097dc2
Chih Cheng Liang <chihchengliang@gmail.com>
2020-10-12 18:33:45 +0800

    renamed: RollupUtils.sol -> ClientFrontend.sol

--- contracts/client/ClientFrontend.sol
+++ contracts/client/ClientFrontend.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 import { Tx } from "../libs/Tx.sol";
 import { Types } from "../libs/Types.sol";
 
-contract RollupUtils {
+contract ClientFrontend {
     using Tx for bytes;
     using Types for Types.UserState;
 
