eb4141471ccf9e72bb1a8678d07ba06b88f4b9ab
Chih Cheng Liang <chihchengliang@gmail.com>
2020-10-13 16:03:46 +0800

    add decode functions

--- contracts/client/Offchain.sol
+++ contracts/client/Offchain.sol
@@ -13,6 +13,24 @@ library Offchain {
         uint256 nonce;
     }
 
+    function decodeTransfer(bytes memory txBytes)
+        internal
+        pure
+        returns (Transfer memory _tx)
+    {
+        (
+            _tx.txType,
+            _tx.fromIndex,
+            _tx.toIndex,
+            _tx.amount,
+            _tx.fee,
+            _tx.nonce
+        ) = abi.decode(
+            txBytes,
+            (uint256, uint256, uint256, uint256, uint256, uint256)
+        );
+    }
+
     struct MassMigration {
         uint256 txType;
         uint256 fromIndex;
@@ -20,6 +38,24 @@ library Offchain {
         uint256 fee;
         uint256 spokeID;
         uint256 nonce;
+    }
+
+    function decodeMassMigration(bytes memory txBytes)
+        internal
+        pure
+        returns (MassMigration memory _tx)
+    {
+        (
+            _tx.txType,
+            _tx.fromIndex,
+            _tx.amount,
+            _tx.fee,
+            _tx.spokeID,
+            _tx.nonce
+        ) = abi.decode(
+            txBytes,
+            (uint256, uint256, uint256, uint256, uint256, uint256)
+        );
     }
 
     struct Create2Transfer {
@@ -30,5 +66,24 @@ library Offchain {
         uint256 amount;
         uint256 fee;
         uint256 nonce;
+    }
+
+    function decodeCreate2Transfer(bytes memory txBytes)
+        internal
+        pure
+        returns (Create2Transfer memory _tx)
+    {
+        (
+            _tx.txType,
+            _tx.fromIndex,
+            _tx.toIndex,
+            _tx.toAccID,
+            _tx.amount,
+            _tx.fee,
+            _tx.nonce
+        ) = abi.decode(
+            txBytes,
+            (uint256, uint256, uint256, uint256, uint256, uint256, uint256)
+        );
     }
 }
