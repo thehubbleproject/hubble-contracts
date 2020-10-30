e2cb4081e938fb2fb1b7235dd3fec824b1f91fa8
Chih Cheng Liang <chihchengliang@gmail.com>
2020-10-30 14:53:21 +0800

    remove MM and C2T API to reduce the contract size while we explore the better interface for all

--- contracts/client/ClientFrontend.sol
+++ contracts/client/ClientFrontend.sol
@@ -22,44 +22,12 @@ contract ClientFrontend {
         return Offchain.decodeTransfer(encodedTx);
     }
 
-    function decodeMassMigration(bytes calldata encodedTx)
-        external
-        pure
-        returns (Offchain.MassMigration memory _tx)
-    {
-        return Offchain.decodeMassMigration(encodedTx);
-    }
-
-    function decodeCreate2Transfer(bytes calldata encodedTx)
-        external
-        pure
-        returns (Offchain.Create2Transfer memory _tx)
-    {
-        return Offchain.decodeCreate2Transfer(encodedTx);
-    }
-
     function encodeTransfer(Offchain.Transfer calldata _tx)
         external
         pure
         returns (bytes memory)
     {
         return Offchain.encodeTransfer(_tx);
-    }
-
-    function encodeMassMigration(Offchain.MassMigration calldata _tx)
-        external
-        pure
-        returns (bytes memory)
-    {
-        return Offchain.encodeMassMigration(_tx);
-    }
-
-    function encodeCreate2Transfer(Offchain.Create2Transfer calldata _tx)
-        external
-        pure
-        returns (bytes memory)
-    {
-        return Offchain.encodeCreate2Transfer(_tx);
     }
 
     function compressTransfer(bytes[] calldata encodedTxs)
@@ -82,45 +50,6 @@ contract ClientFrontend {
         return Tx.serialize(txTxs);
     }
 
-    function compressMassMigration(bytes[] calldata encodedTxs)
-        external
-        pure
-        returns (bytes memory)
-    {
-        Tx.MassMigration[] memory txTxs = new Tx.MassMigration[](
-            encodedTxs.length
-        );
-        for (uint256 i = 0; i < txTxs.length; i++) {
-            Offchain.MassMigration memory _tx = Offchain.decodeMassMigration(
-                encodedTxs[i]
-            );
-            txTxs[i] = Tx.MassMigration(_tx.fromIndex, _tx.amount, _tx.fee);
-        }
-        return Tx.serialize(txTxs);
-    }
-
-    function compressCreate2Transfer(bytes[] calldata encodedTxs)
-        external
-        pure
-        returns (bytes memory)
-    {
-        Tx.Create2Transfer[] memory txTxs = new Tx.Create2Transfer[](
-            encodedTxs.length
-        );
-        for (uint256 i = 0; i < txTxs.length; i++) {
-            Offchain.Create2Transfer memory _tx = Offchain
-                .decodeCreate2Transfer(encodedTxs[i]);
-            txTxs[i] = Tx.Create2Transfer(
-                _tx.fromIndex,
-                _tx.toIndex,
-                _tx.toAccID,
-                _tx.amount,
-                _tx.fee
-            );
-        }
-        return Tx.serialize(txTxs);
-    }
-
     function decompressTransfer(bytes calldata txs)
         external
         pure
@@ -130,32 +59,6 @@ contract ClientFrontend {
         Tx.Transfer[] memory txTxs = new Tx.Transfer[](size);
         for (uint256 i = 0; i < size; i++) {
             txTxs[i] = txs.transferDecode(i);
-        }
-        return txTxs;
-    }
-
-    function decompressMassMigration(bytes calldata txs)
-        external
-        pure
-        returns (Tx.MassMigration[] memory txTxs)
-    {
-        uint256 size = txs.massMigrationSize();
-        Tx.MassMigration[] memory txTxs = new Tx.MassMigration[](size);
-        for (uint256 i = 0; i < size; i++) {
-            txTxs[i] = txs.massMigrationDecode(i);
-        }
-        return txTxs;
-    }
-
-    function decompressCreate2Transfer(bytes calldata txs)
-        external
-        pure
-        returns (Tx.Create2Transfer[] memory txTxs)
-    {
-        uint256 size = txs.create2TransferSize();
-        Tx.Create2Transfer[] memory txTxs = new Tx.Create2Transfer[](size);
-        for (uint256 i = 0; i < size; i++) {
-            txTxs[i] = txs.create2TransferDecode(i);
         }
         return txTxs;
     }
@@ -178,63 +81,6 @@ contract ClientFrontend {
         bytes memory txMsg = Tx.transferMessageOf(txTx, _tx.nonce);
         uint256[2] memory message = BLS.hashToPoint(domain, txMsg);
         require(BLS.verifySingle(signature, pubkey, message), "Bad Signature");
-    }
-
-    function valiateMassMigration(
-        bytes calldata encodedTx,
-        uint256[2] calldata signature,
-        uint256[4] calldata pubkey,
-        bytes32 domain
-    ) external view {
-        Offchain.MassMigration memory _tx = Offchain.decodeMassMigration(
-            encodedTx
-        );
-        Tx.encodeDecimal(_tx.amount);
-        Tx.encodeDecimal(_tx.fee);
-        Tx.MassMigration memory txTx = Tx.MassMigration(
-            _tx.fromIndex,
-            _tx.amount,
-            _tx.fee
-        );
-        bytes memory txMsg = Tx.massMigrationMessageOf(
-            txTx,
-            _tx.nonce,
-            _tx.spokeID
-        );
-        uint256[2] memory message = BLS.hashToPoint(domain, txMsg);
-        require(BLS.verifySingle(signature, pubkey, message), "Bad Signature");
-    }
-
-    function valiateCreate2Transfer(
-        bytes calldata encodedTx,
-        uint256[2] calldata signature,
-        uint256[4] calldata fromPubkey,
-        uint256[4] calldata toPubkey,
-        bytes32 domain
-    ) external view {
-        Offchain.Create2Transfer memory _tx = Offchain.decodeCreate2Transfer(
-            encodedTx
-        );
-        Tx.encodeDecimal(_tx.amount);
-        Tx.encodeDecimal(_tx.fee);
-        Tx.Create2Transfer memory txTx = Tx.Create2Transfer(
-            _tx.fromIndex,
-            _tx.toIndex,
-            _tx.toAccID,
-            _tx.amount,
-            _tx.fee
-        );
-        bytes memory txMsg = Tx.create2TransferMessageOf(
-            txTx,
-            _tx.nonce,
-            fromPubkey,
-            toPubkey
-        );
-        uint256[2] memory message = BLS.hashToPoint(domain, txMsg);
-        require(
-            BLS.verifySingle(signature, fromPubkey, message),
-            "Bad Signature"
-        );
     }
 
     function validateAndApplyTransfer(
@@ -269,69 +115,6 @@ contract ClientFrontend {
         return (sender.encode(), receiver.encode(), result);
     }
 
-    function validateAndApplyMassMigration(
-        bytes calldata senderEncoded,
-        bytes calldata encodedTx
-    )
-        external
-        pure
-        returns (
-            bytes memory newSender,
-            bytes memory withdrawState,
-            Types.Result result
-        )
-    {
-        Offchain.MassMigration memory _tx = Offchain.decodeMassMigration(
-            encodedTx
-        );
-        Types.UserState memory sender = Types.decodeState(senderEncoded);
-        (sender, result) = Transition.validateAndApplySender(
-            sender.tokenType,
-            _tx.amount,
-            _tx.fee,
-            sender
-        );
-        if (result != Types.Result.Ok) return (sender.encode(), "", result);
-        withdrawState = Transition.createState(
-            sender.pubkeyIndex,
-            sender.tokenType,
-            _tx.amount
-        );
-        return (sender.encode(), withdrawState, Types.Result.Ok);
-    }
-
-    function validateAndApplyCreate2Transfer(
-        bytes calldata senderEncoded,
-        bytes calldata encodedTx
-    )
-        external
-        pure
-        returns (
-            bytes memory newSender,
-            bytes memory newReceiver,
-            Types.Result result
-        )
-    {
-        Offchain.Create2Transfer memory _tx = Offchain.decodeCreate2Transfer(
-            encodedTx
-        );
-
-        Types.UserState memory sender = Types.decodeState(senderEncoded);
-        (sender, result) = Transition.validateAndApplySender(
-            sender.tokenType,
-            _tx.amount,
-            _tx.fee,
-            sender
-        );
-        if (result != Types.Result.Ok) return (sender.encode(), "", result);
-        newReceiver = Transition.createState(
-            _tx.toAccID,
-            sender.tokenType,
-            _tx.amount
-        );
-        return (sender.encode(), newReceiver, Types.Result.Ok);
-    }
-
     function processTransfer(
         bytes32 stateRoot,
         bytes memory encodedTx,
@@ -351,58 +134,6 @@ contract ClientFrontend {
         return Transition.processTransfer(stateRoot, _tx, tokenType, from, to);
     }
 
-    function processMassMigration(
-        bytes32 stateRoot,
-        bytes memory encodedTx,
-        uint256 tokenType,
-        Types.StateMerkleProof memory from
-    )
-        public
-        pure
-        returns (
-            bytes32 newRoot,
-            bytes memory freshState,
-            Types.Result result
-        )
-    {
-        Offchain.MassMigration memory offchainTx = Offchain.decodeMassMigration(
-            encodedTx
-        );
-        Tx.MassMigration memory _tx = Tx.MassMigration(
-            offchainTx.fromIndex,
-            offchainTx.amount,
-            offchainTx.fee
-        );
-        return Transition.processMassMigration(stateRoot, _tx, tokenType, from);
-    }
-
-    function processCreate2Transfer(
-        bytes32 stateRoot,
-        bytes memory encodedTx,
-        uint256 tokenType,
-        Types.StateMerkleProof memory from,
-        Types.StateMerkleProof memory to
-    ) public pure returns (bytes32 newRoot, Types.Result result) {
-        Offchain.Create2Transfer memory offchainTx = Offchain
-            .decodeCreate2Transfer(encodedTx);
-        Tx.Create2Transfer memory _tx = Tx.Create2Transfer(
-            offchainTx.fromIndex,
-            offchainTx.toIndex,
-            offchainTx.toAccID,
-            offchainTx.amount,
-            offchainTx.fee
-        );
-
-        return
-            Transition.processCreate2Transfer(
-                stateRoot,
-                _tx,
-                tokenType,
-                from,
-                to
-            );
-    }
-
     function checkSignatureTransfer(
         uint256[2] memory signature,
         Types.SignatureProof memory proof,
@@ -413,46 +144,6 @@ contract ClientFrontend {
     ) public view returns (Types.Result) {
         return
             Authenticity.verifyTransfer(
-                signature,
-                proof,
-                stateRoot,
-                accountRoot,
-                domain,
-                txs
-            );
-    }
-
-    function checkSignatureMassMigration(
-        uint256[2] memory signature,
-        Types.SignatureProof memory proof,
-        bytes32 stateRoot,
-        bytes32 accountRoot,
-        bytes32 domain,
-        uint256 spokeID,
-        bytes memory txs
-    ) public view returns (Types.Result) {
-        return
-            Authenticity.verifyMassMigration(
-                signature,
-                proof,
-                stateRoot,
-                accountRoot,
-                domain,
-                spokeID,
-                txs
-            );
-    }
-
-    function checkSignatureCreate2Transfer(
-        uint256[2] memory signature,
-        Types.SignatureProofWithReceiver memory proof,
-        bytes32 stateRoot,
-        bytes32 accountRoot,
-        bytes32 domain,
-        bytes memory txs
-    ) public view returns (Types.Result) {
-        return
-            Authenticity.verifyCreate2Transfer(
                 signature,
                 proof,
                 stateRoot,
--- test/clientFrontend.test.ts
+++ test/clientFrontend.test.ts
@@ -1,10 +1,5 @@
 import { assert } from "chai";
-import {
-    serialize,
-    TxCreate2Transfer,
-    TxMassMigration,
-    TxTransfer
-} from "../ts/tx";
+import { serialize, TxTransfer } from "../ts/tx";
 import { ethers } from "@nomiclabs/buidler";
 import { ClientFrontend } from "../types/ethers-contracts/ClientFrontend";
 import { ClientFrontendFactory } from "../types/ethers-contracts";
@@ -42,47 +37,6 @@ describe("Client Frontend", async function() {
             serialize(txs)
         );
     });
-    it("MassMigration", async function() {
-        const txs = [];
-        const txsEncoded = [];
-        for (let i = 0; i < 100; i++) {
-            const tx = TxMassMigration.rand();
-            const encodedTx = tx.encodeOffchain();
-            const _tx = await contract.decodeMassMigration(tx.encodeOffchain());
-            assert.equal(_tx.fromIndex.toNumber(), tx.fromIndex);
-            assert.equal(_tx.amount.toString(), tx.amount.toString());
-            assert.equal(_tx.fee.toString(), tx.fee.toString());
-            assert.equal(_tx.spokeID.toNumber(), tx.spokeID);
-            assert.equal(_tx.nonce.toNumber(), tx.nonce);
-            txs.push(tx);
-            txsEncoded.push(encodedTx);
-        }
-        assert.equal(
-            await contract.compressMassMigration(txsEncoded),
-            serialize(txs)
-        );
-    });
-    it("Create2Transfer", async function() {
-        const txs = [];
-        const txsEncoded = [];
-        for (let i = 0; i < 100; i++) {
-            const tx = TxCreate2Transfer.rand();
-            const encodedTx = tx.encodeOffchain();
-            const _tx = await contract.decodeCreate2Transfer(encodedTx);
-            assert.equal(_tx.fromIndex.toNumber(), tx.fromIndex);
-            assert.equal(_tx.toIndex.toNumber(), tx.toIndex);
-            assert.equal(_tx.toAccID.toNumber(), tx.toAccID);
-            assert.equal(_tx.amount.toString(), tx.amount.toString());
-            assert.equal(_tx.fee.toString(), tx.fee.toString());
-            assert.equal(_tx.nonce.toNumber(), tx.nonce);
-            txs.push(tx);
-            txsEncoded.push(encodedTx);
-        }
-        assert.equal(
-            await contract.compressCreate2Transfer(txsEncoded),
-            serialize(txs)
-        );
-    });
 
     it("Validate transfer", async function() {
         const tx = TxTransfer.rand();
@@ -92,33 +46,6 @@ describe("Client Frontend", async function() {
             tx.encodeOffchain(),
             mcl.g1ToHex(signature),
             user.getPubkey(),
-            DOMAIN
-        );
-    });
-
-    it("Validate MassMigration", async function() {
-        const tx = TxMassMigration.rand();
-        const user = State.new(0, 0, 0, 0).newKeyPair();
-        const signature = user.sign(tx);
-        await contract.valiateMassMigration(
-            tx.encodeOffchain(),
-            mcl.g1ToHex(signature),
-            user.getPubkey(),
-            DOMAIN
-        );
-    });
-    it("Validate create2Transfer", async function() {
-        const tx = TxCreate2Transfer.rand();
-        const sender = State.new(0, 0, 0, 0).newKeyPair();
-        const receiver = State.new(0, 0, 0, 0).newKeyPair();
-        tx.fromPubkey = sender.getPubkey();
-        tx.toPubkey = receiver.getPubkey();
-        const signature = sender.sign(tx);
-        await contract.valiateCreate2Transfer(
-            tx.encodeOffchain(),
-            mcl.g1ToHex(signature),
-            tx.fromPubkey,
-            tx.toPubkey,
             DOMAIN
         );
     });
