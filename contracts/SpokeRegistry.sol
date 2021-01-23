804b61f6914ecb855844e1d8cd46acb6fab4f03e
Chih Cheng Liang <chihchengliang@gmail.com>
2021-01-23 22:34:02 +0800

    nitpick on spoke registry's visibility

--- contracts/SpokeRegistry.sol
+++ contracts/SpokeRegistry.sol
@@ -4,12 +4,12 @@ contract SpokeRegistry {
     mapping(uint256 => address) public registeredSpokes;
     uint256 public numSpokes;
 
-    function registerSpoke(address spokeContract) public {
+    function registerSpoke(address spokeContract) external {
         numSpokes++;
         registeredSpokes[numSpokes] = spokeContract;
     }
 
-    function getSpokeAddress(uint256 spokeID) external returns (address) {
+    function getSpokeAddress(uint256 spokeID) external view returns (address) {
         return registeredSpokes[spokeID];
     }
 }
