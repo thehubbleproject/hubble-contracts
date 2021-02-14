1428fbce899cf3770ee03180141dbcdb8a85e415
Chih Cheng Liang <chihchengliang@gmail.com>
2021-02-14 21:23:17 +0800

    fix decimal

--- test/fast/decimal.test.ts
+++ test/fast/decimal.test.ts
@@ -1,26 +1,40 @@
 import { assert } from "chai";
-import { USDT } from "../../ts/decimal";
+import { float16, USDT } from "../../ts/decimal";
 
 describe("Decimal", () => {
-    const goodCases: number[] = [0, 1, 10000, 12.13, 0.1234, 18690000000];
-
     it("Get's some property right", () => {
-        assert.equal(USDT.bytesLength, 2);
+        assert.equal(float16.bytesLength, 2);
     });
 
-    it("casts values", () => {
-        for (const value of goodCases) {
+    it("Rounding values", () => {
+        const losslessCases: number[] = [
+            0,
+            1,
+            10000,
+            12.13,
+            0.1234,
+            18690000000
+        ];
+        for (const value of losslessCases) {
+            const valueBN = USDT.parse(value.toString());
             assert.equal(
-                USDT.cast(value),
-                value,
+                float16.round(valueBN).toString(),
+                valueBN.toString(),
                 "Casted value should be the same in good cases"
             );
         }
-        assert.equal(USDT.cast(0.12345), 0.1234);
-        assert.equal(USDT.cast(56789), 56700);
-        assert.equal(USDT.cast(123.123), 123.1);
-        assert.equal(USDT.cast(186950000000), 186900000000);
-        assert.equal(USDT.cast(4096), 4090);
-        assert.equal(USDT.cast(4095), 4095);
+        const lossyCases = [
+            { input: 0.12345, expect: 0.1234 },
+            { input: 56789, expect: 56700 },
+            { input: 123.123, expect: 123.1 },
+            { input: 186950000000, expect: 186900000000 },
+            { input: 4096, expect: 4090 },
+            { input: 4095, expect: 4095 }
+        ];
+        for (const _case of lossyCases) {
+            const valueBN = USDT.parse(_case.input.toString());
+            const valueRounded = float16.round(valueBN);
+            assert.equal(Number(USDT.format(valueRounded)), _case.expect);
+        }
     });
 });
