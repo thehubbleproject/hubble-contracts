6965ac4f53edbbd53d7bb013ae45f91d69a9a80a
Chih Cheng Liang <chihchengliang@gmail.com>
2021-02-14 21:22:51 +0800

    make ERC20 a class, remove DecimalCodec

--- ts/decimal.ts
+++ ts/decimal.ts
@@ -3,31 +3,27 @@ import { formatUnits, hexZeroPad, parseUnits } from "ethers/lib/utils";
 import { EncodingError } from "./exceptions";
 import { randHex } from "./utils";
 
-/**
- * Parse the human readable value like "1.23" to the ERC20 integer amount.
- * @param humanValue could be a fractional number but is string. Like '1.23'
- * @param decimals The ERC20 decimals()
- * @returns the ERC20 integer amount. Like '1230000000000000000' but in BigNumber
- */
-export function parseERC20(
-    humanValue: string,
-    decimals: number = 18
-): BigNumber {
-    return parseUnits(humanValue, decimals);
+export class ERC20 {
+    constructor(public decimals: number = 18) {}
+    /**
+     * Parse the human readable value like "1.23" to the ERC20 integer amount.
+     * @param humanValue could be a fractional number but is string. Like '1.23'
+     * @returns the ERC20 integer amount. Like '1230000000000000000' but in BigNumber
+     */
+    parse(humanValue: string) {
+        return parseUnits(humanValue, this.decimals);
+    }
+    /**
+     * Format the ERC20 integer amount to human readable value.
+     * @param uint256Value is an integer like '1230000000000000000'
+     * @returns Could be a fractional number but in string. Like '1.23'
+     */
+    format(uint256Value: BigNumberish) {
+        return formatUnits(uint256Value, this.decimals);
+    }
 }
 
-/**
- * Format the ERC20 integer amount to human readable value.
- * @param uint256Value is an integer like '1230000000000000000'
- * @param decimals The ERC20 decimals()
- * @returns Could be a fractional number but in string. Like '1.23'
- */
-export function formatERC20(
-    uint256Value: BigNumberish,
-    decimals: number = 18
-): string {
-    return formatUnits(uint256Value, decimals);
-}
+export const USDT = new ERC20(6);
 
 export class Float {
     private mantissaMax: BigNumber;
@@ -91,97 +87,4 @@ export class Float {
     }
 }
 
-export const float2 = new Float(4, 12);
-
-export class DecimalCodec {
-    private mantissaMax: BigNumber;
-    private exponentMax: number;
-    private exponentMask: BigNumber;
-    public bytesLength: number;
-
-    constructor(
-        public readonly exponentBits: number,
-        public readonly mantissaBits: number,
-        public readonly place: number
-    ) {
-        this.mantissaMax = BigNumber.from(2 ** mantissaBits - 1);
-        this.exponentMax = 2 ** exponentBits - 1;
-        this.exponentMask = BigNumber.from(this.exponentMax << mantissaBits);
-        this.bytesLength = (mantissaBits + exponentBits) / 8;
-    }
-    public rand(): string {
-        return randHex(this.bytesLength);
-    }
-
-    public randInt(): BigNumber {
-        return this.decodeInt(this.rand());
-    }
-
-    /**
-     * Given an arbitrary js number returns a js number that can be encoded.
-     */
-    public cast(input: number): number {
-        if (input == 0) {
-            return input;
-        }
-
-        const logMantissaMax = Math.log10(this.mantissaMax.toNumber());
-        const logInput = Math.log10(input);
-        const exponent = Math.floor(logMantissaMax - logInput);
-        const mantissa = Math.floor(input * 10 ** exponent);
-
-        return mantissa / 10 ** exponent;
-    }
-
-    /**
-     * Given an arbitrary js number returns a integer that can be encoded
-     */
-    public castInt(input: number): BigNumber {
-        const validNum = this.cast(input);
-        return BigNumber.from(Math.round(validNum * 10 ** this.place));
-    }
-
-    /**
-     * Find a BigNumber that's less than the input and compressable
-     */
-    public castBigNumber(input: BigNumber): BigNumber {
-        let mantissa = input;
-        for (let exponent = 0; exponent < this.exponentMax; exponent++) {
-            if (mantissa.lte(this.mantissaMax))
-                return mantissa.mul(BigNumber.from(10).pow(exponent));
-            mantissa = mantissa.div(10);
-        }
-        throw new EncodingError(`Can't cast input ${input.toString()}`);
-    }
-
-    public encodeInt(input: BigNumberish): string {
-        let exponent = 0;
-        let mantissa = BigNumber.from(input.toString());
-        for (let i = 0; i < this.exponentMax; i++) {
-            if (!mantissa.isZero() && mantissa.mod(10).isZero()) {
-                mantissa = mantissa.div(10);
-                exponent += 1;
-            } else {
-                break;
-            }
-        }
-        if (mantissa.gt(this.mantissaMax)) {
-            throw new EncodingError(
-                `Can not encode input ${input}, mantissa ${mantissa} should not be larger than ${this.mantissaMax}`
-            );
-        }
-        const hex = BigNumber.from(exponent)
-            .shl(this.mantissaBits)
-            .add(mantissa)
-            .toHexString();
-        return ethers.utils.hexZeroPad(hex, this.bytesLength);
-    }
-    public decodeInt(input: BytesLike): BigNumber {
-        const mantissa = this.mantissaMax.and(input);
-        const exponent = this.exponentMask.and(input).shr(this.mantissaBits);
-
-        return mantissa.mul(BigNumber.from(10).pow(exponent));
-    }
-}
-
-export const USDT = new DecimalCodec(4, 12, 6);
+export const float16 = new Float(4, 12);
