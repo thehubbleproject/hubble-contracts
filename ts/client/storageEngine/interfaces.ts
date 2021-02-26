a27080995aae8929a9401e1d1f4e2870e5c149b2
Chih Cheng Liang <chihchengliang@gmail.com>
2021-02-26 22:37:45 +0800

    add StorageEngine and MemoryEngine

---
+++ ts/client/storageEngine/interfaces.ts
@@ -0,0 +1,15 @@
+export interface WithWitness<Item> {
+    item: Item;
+    witness: string[];
+}
+
+export interface StorageEngine<Item> {
+    get(itemID: number): Promise<Item>;
+    update(itemID: number, item: Item): Promise<void>;
+    create(itemID: number, item: Item): Promise<void>;
+    getWithWitness(itemID: number): Promise<WithWitness<Item>>;
+    getCheckpoint(): number;
+    revert(checkpoint?: number): void;
+    commit(): Promise<void>;
+    root: string;
+}
