9e12fc9cc4d448d5d51687b4865e38fa1485e6bc
Chih Cheng Liang <chihchengliang@gmail.com>
2021-03-10 23:01:48 +0800

    handle the case if more subtree roots are queued

--- ts/client/features/deposit.ts
+++ ts/client/features/deposit.ts
@@ -9,19 +9,29 @@ interface Subtree {
 
 export class DepositPool {
     private depositLeaves: State[];
+    private subtreeQueue: Subtree[];
     constructor(public readonly paramMaxSubtreeSize: number) {
         this.depositLeaves = [];
+        this.subtreeQueue = [];
     }
 
     pushDeposit(encodedState: BytesLike) {
         const state = State.fromEncoded(encodedState);
         this.depositLeaves.push(state);
+        if (this.depositLeaves.length >= this.paramMaxSubtreeSize) {
+            this.pushSubtree();
+        }
     }
-
-    popDepositSubtree(): Subtree {
+    private pushSubtree() {
         const states = this.depositLeaves.slice();
         const root = Tree.merklize(states.map(s => s.hash())).root;
         this.depositLeaves = [];
-        return { states, root };
+        this.subtreeQueue.push({ states, root });
+    }
+
+    popDepositSubtree(): Subtree {
+        const subtree = this.subtreeQueue.shift();
+        if (!subtree) throw new Error("no subtre available");
+        return subtree;
     }
 }
