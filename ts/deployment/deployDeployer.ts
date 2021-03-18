d1d20194a0f518f7a765b337b72094415489a644
Chih Cheng Liang <chihchengliang@gmail.com>
2021-03-19 01:04:18 +0800

    revamp keyless deployer

--- ts/deployment/deployDeployer.ts
+++ ts/deployment/deployDeployer.ts
@@ -1,8 +1,6 @@
 import { Signer } from "ethers";
-import { BigNumber } from "ethers";
 import assert from "assert";
-import { keylessDeploy, calculateKeylessDeployment } from "./keylessDeployment";
-import { Provider } from "@ethersproject/providers";
+import { KeylessDeployer } from "./keylessDeployment";
 import {
     deployerBytecode,
     DEPLOYER_ADDRESS,
@@ -10,71 +8,31 @@ import {
 } from "./static";
 import { logAddress, logDeployment } from "../../scripts/logger";
 
-export async function calculateDeployerAddress(
-    provider?: Provider
-): Promise<{ deployerAddress: string; keylessAccount: string }> {
-    let result = await calculateKeylessDeployment(
-        provider,
-        deployerBytecode(),
-        KEYLESS_DEPLOYMENT.GAS_PRICE,
-        KEYLESS_DEPLOYMENT.GAS_LIMIT,
-        false
-    );
-    return {
-        deployerAddress: result.contractAddress,
-        keylessAccount: result.keylessAccount
-    };
-}
-
-export async function calculateGasLimit(
-    provider: Provider
-): Promise<BigNumber> {
-    let result = await calculateKeylessDeployment(
-        provider,
-        deployerBytecode(),
-        KEYLESS_DEPLOYMENT.GAS_PRICE,
-        KEYLESS_DEPLOYMENT.GAS_LIMIT,
-        false
-    );
-    return result.estimatedGasCost;
-}
-
 export async function deployDeployer(
     signer: Signer,
     verbose: boolean
 ): Promise<boolean> {
     assert(signer.provider);
     const provider = signer.provider;
-    const result = await calculateKeylessDeployment(
-        provider,
-        deployerBytecode(),
-        KEYLESS_DEPLOYMENT.GAS_PRICE,
-        KEYLESS_DEPLOYMENT.GAS_LIMIT,
-        verbose
-    );
+    const deployer = new KeylessDeployer(deployerBytecode()).connect(provider);
 
-    if (result.alreadyDeployed) {
+    if (await deployer.alreadyDeployed()) {
         logAddress(
             verbose,
             "Deployer is ALREADY deployed",
-            result.contractAddress
+            deployer.contractAddress
         );
         return true;
     }
-    assert(KEYLESS_DEPLOYMENT.GAS_LIMIT.gte(result.estimatedGasCost));
-    assert(DEPLOYER_ADDRESS == result.contractAddress);
-    const _result = await keylessDeploy(
-        signer,
-        deployerBytecode(),
-        KEYLESS_DEPLOYMENT.GAS_PRICE,
-        KEYLESS_DEPLOYMENT.GAS_LIMIT,
-        verbose
-    );
+    assert(KEYLESS_DEPLOYMENT.GAS_LIMIT.gte(await deployer.estimateGas()));
+    assert(DEPLOYER_ADDRESS == deployer.contractAddress);
+
+    const receipt = await deployer.fundAndDeploy(signer);
     logDeployment(
         verbose,
         "Deployed: Deployer",
-        _result.receipt.transactionHash,
-        _result.contractAddress
+        receipt.transactionHash,
+        deployer.contractAddress
     );
     return true;
 }
