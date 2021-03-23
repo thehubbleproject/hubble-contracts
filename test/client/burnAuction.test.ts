14d9d2e84f4f3ae3e292e054293812f9c4fb2430
Chih Cheng Liang <chihchengliang@gmail.com>
2021-03-24 02:06:29 +0800

    update test

--- test/client/burnAuction.test.ts
+++ test/client/burnAuction.test.ts
@@ -2,6 +2,7 @@ import { Signer } from "@ethersproject/abstract-signer";
 import { BigNumber } from "@ethersproject/bignumber";
 import { assert } from "chai";
 import { ethers } from "hardhat";
+import { BurnAuctionWrapper } from "../../ts/burnAuction";
 import { Bidder } from "../../ts/client/services/bidder";
 import { PRODUCTION_PARAMS } from "../../ts/constants";
 import { mineBlocks } from "../../ts/utils";
@@ -12,37 +13,38 @@ describe("Bidder", function() {
     let deployer: Signer;
     let serviceSigner: Signer;
     let competitor: Signer;
-    let burnAuction: BurnAuction;
+    let contract: BurnAuction;
+    let burnAuction: BurnAuctionWrapper;
     let bidder: Bidder;
     const willingnessToBid = BigNumber.from("1");
 
     beforeEach(async function() {
         [deployer, serviceSigner, competitor] = await ethers.getSigners();
-        burnAuction = await new BurnAuctionFactory(deployer).deploy(
+        contract = await new BurnAuctionFactory(deployer).deploy(
             PRODUCTION_PARAMS.DONATION_ADDRESS,
             PRODUCTION_PARAMS.DONATION_NUMERATOR
         );
-        const initialBlocks = await burnAuction.DELTA_BLOCKS_INITIAL_SLOT();
-        const blocksPerSlot = await burnAuction.BLOCKS_PER_SLOT();
+        burnAuction = await BurnAuctionWrapper.fromContract(contract);
+
         bidder = await Bidder.new(
             willingnessToBid,
-            burnAuction.connect(serviceSigner)
+            contract.connect(serviceSigner)
         );
-        await mineBlocks(ethers.provider, initialBlocks + blocksPerSlot);
+        const nblocks = burnAuction.initialBlocks + burnAuction.blocksPerSlot;
+        await mineBlocks(ethers.provider, nblocks);
     });
 
     it("bids", async function() {
-        const currentSlot = await burnAuction.currentSlot();
         const currentBlock = await ethers.provider.getBlockNumber();
         await bidder.maybeBid(currentBlock);
         assert.equal(
-            (await burnAuction.auction(currentSlot + 2)).coordinator,
+            (await burnAuction.getAuctioningSlotBid()).coordinator,
             await serviceSigner.getAddress()
         );
     });
 
     it("doesn't bid if already win", async function() {
-        await burnAuction.connect(serviceSigner).bid("1", { value: "1" });
+        await contract.connect(serviceSigner).bid("1", { value: "1" });
         const currentBlock = await ethers.provider.getBlockNumber();
         // bid would result an error and thus fail the test
         await bidder.maybeBid(currentBlock);
@@ -50,12 +52,11 @@ describe("Bidder", function() {
 
     it("doesn't bid if amount is too much", async function() {
         const amount = willingnessToBid.add("1");
-        await burnAuction.connect(competitor).bid(amount, { value: amount });
+        await contract.connect(competitor).bid(amount, { value: amount });
         const currentBlock = await ethers.provider.getBlockNumber();
         await bidder.maybeBid(currentBlock);
-        const currentSlot = await burnAuction.currentSlot();
         assert.equal(
-            (await burnAuction.auction(currentSlot + 2)).coordinator,
+            (await burnAuction.getAuctioningSlotBid()).coordinator,
             await competitor.getAddress()
         );
     });
