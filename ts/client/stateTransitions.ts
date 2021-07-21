72ed8314099ad1895cbfc234fcc139a3a9616504
ChihChengLiang <chihchengliang@gmail.com>
2021-07-21 10:23:29 +0800

    remove dead code processTransferCommit

--- ts/client/stateTransitions.ts
+++ ts/client/stateTransitions.ts
@@ -1,8 +1,6 @@
 import { BigNumber } from "ethers";
 import { ZeroAmount, InsufficientFund, WrongTokenID } from "../exceptions";
 import { State } from "../state";
-import { TxTransfer } from "../tx";
-import { sum } from "../utils";
 import { StateStorageEngine } from "./storageEngine";
 
 function applySender(sender: State, decrement: BigNumber): State {
@@ -66,47 +64,4 @@ export async function processReceiver(
     validateReceiver(state, tokenID);
     const postState = applyReceiver(state, increment);
     await engine.update(receiverID.toNumber(), postState);
-}
-
-async function processTransfer(
-    tx: TxTransfer,
-    tokenID: BigNumber,
-    engine: StateStorageEngine
-): Promise<void> {
-    await processSender(
-        BigNumber.from(tx.fromIndex),
-        tokenID,
-        tx.amount,
-        tx.fee,
-        engine
-    );
-    await processReceiver(
-        BigNumber.from(tx.toIndex),
-        tx.amount,
-        tokenID,
-        engine
-    );
-}
-
-export async function processTransferCommit(
-    txs: TxTransfer[],
-    feeReceiverID: number,
-    engine: StateStorageEngine
-): Promise<TxTransfer[]> {
-    const tokenID = (await engine.get(feeReceiverID)).tokenID;
-    let acceptedTxs = [];
-    for (const tx of txs) {
-        const checkpoint = engine.getCheckpoint();
-        try {
-            await processTransfer(tx, tokenID, engine);
-            engine.commit();
-            acceptedTxs.push(tx);
-        } catch (err) {
-            console.log("Drop tx due to ", err.message);
-            engine.revert(checkpoint);
-        }
-    }
-    const fees = sum(acceptedTxs.map(tx => tx.fee));
-    await processReceiver(BigNumber.from(feeReceiverID), fees, tokenID, engine);
-    return acceptedTxs;
 }
