b7bf3d2f8f7f598f4bd4a01708019c1c1f8d1307
Maxim Sidorov <max.sidorov@ottofeller.com>
2021-12-16 16:32:26 +0300

    add RegisteredSpoke event

--- contracts/SpokeRegistry.sol
+++ contracts/SpokeRegistry.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.6.12;
 
 contract SpokeRegistry {
+    event RegisteredSpoke(uint256 spokeID, address spokeContract);
+
     mapping(uint256 => address) public registeredSpokes;
     uint256 public numSpokes;
 
     function registerSpoke(address spokeContract) external {
         numSpokes++;
         registeredSpokes[numSpokes] = spokeContract;
+        emit RegisteredSpoke(numSpokes, spokeContract);
     }
 
     function getSpokeAddress(uint256 spokeID) external view returns (address) {
