6e524a216023a50fe1ab62b0f6a4767a53559c7a
Daniel Izdebski <spidiritsu@gmail.com>
2022-02-18 11:15:37 +0100

    Remove comment

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -331,7 +331,6 @@ describe("Integration Test", function() {
             const tree = migrationTrees[i];
             const { user, index } = group.pickRandom();
             const signature = user.signRaw(withdrawerAddress).sol;
-            // The new stateID in the migration tree is the position user in the group
             const withdrawProof = tree.getWithdrawProof(user.stateID, index);
             await withdrawManager
                 .connect(withdrawer)
